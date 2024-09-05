571779370476947225ea5fefc6ad6f92aee1a856
sachinshahi754 <80482579+sachinshahi754@users.noreply.github.com>
2024-09-05 07:53:50 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    const num = 5;
+  return (
+    
+    <>
+    <h1>Navbar</h1>
+    <p1>This is a Navbar</p1>
+   </>
+    
+  )
+}
+
+export default Navbar
